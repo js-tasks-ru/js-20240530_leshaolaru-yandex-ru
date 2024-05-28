8e31e7cc53f756affa100b84bf3e93cd7cc27494
Lesha Olaru <leshaolaru@yandex.ru>
2024-05-28 18:56:25 +0300

    Task №2.3

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,15 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+  const newFields = Object.keys(obj).filter(key => !fields.includes(key));
+  const newObj = {};
 
+  Object.keys(obj)
+    .forEach(key => {
+      if (newFields.includes(key)) {
+        newObj[key] = key;
+      }
+    });
+
+  return newObj;
 };
