a3ed0b720179ea38c7a59f6d274ab5cd1483f8bd
Lesha Olaru <leshaolaru@yandex.ru>
2024-05-28 20:06:02 +0300

    Task №2.1

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -6,4 +6,29 @@
  */
 export function sortStrings(arr, param = 'asc') {
 
+  let newArr = arr.slice();
+
+  if (param === 'desc') {
+    return newArr
+      .sort((a, b) => compareIncludeRegister(a, b))
+      .reverse();
+  }
+
+  return newArr.sort((a, b) => compareIncludeRegister(a, b));
+}
+
+function compareIncludeRegister(word1, word2) {
+  if (isDifferentWordsJustFirsLetter(word1, word2)) {
+    if (word1.charAt(0) === word2.charAt(0).toUpperCase()) {
+      return -1;
+    } else {
+      return 1;
+    }
+  }
+
+  return word1.localeCompare(word2, "ru");
+}
+
+function isDifferentWordsJustFirsLetter(word1, word2) {
+  return (word1.toUpperCase() === word2.toUpperCase()) && (word1.charAt(0) !== word2.charAt(0));
 }
