98e86af9f6cbbcad1531b50d53309cfec8332a98
Lesha Olaru <leshaolaru@yandex.ru>
2024-06-02 15:26:33 +0300

    task trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,25 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  return getSameSymbolGroup(string)
+    .map(value => (value.length > size) ? value.substring(0, size) : value)
+    .join("");
+}
 
+function getSameSymbolGroup(str) {
+  let arr = str.split("");
+  let s = arr[0];
+  let result = [];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i - 1] === arr[i]) {
+      s = s + arr[i];
+    } else {
+      result.push(s);
+      s = arr[i];
+    }
+    if (i === (arr.length - 1)) {
+      result.push(s);
+    }
+  }
+  return result;
 }
