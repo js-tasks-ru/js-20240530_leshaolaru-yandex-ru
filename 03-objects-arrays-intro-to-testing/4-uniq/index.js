0e3e840fffa871e28f4874f927835ff88b908b6e
Lesha Olaru <leshaolaru@yandex.ru>
2024-06-02 22:52:22 +0300

    task uniq

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,8 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-
+  let set = new Set(arr);
+  let newArr = [];
+  set.forEach(value => newArr.push(value));
+  return newArr;
 }
